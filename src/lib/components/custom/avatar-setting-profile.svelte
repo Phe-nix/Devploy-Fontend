c521fab08e0df8d07b25a91654d151f372e1c1cc
Thavith Javisooth <spicedexdee123@gmail.com>
2024-07-15 17:59:55 +0700

    🚩 add lib

---
+++ src/lib/components/custom/avatar-setting-profile.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    //import shadcn-svelte
+    import * as Avatar from "$lib/components/ui/avatar";
+</script>
+
+<Avatar.Root>
+    <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+    <Avatar.Fallback>CN</Avatar.Fallback>
+  </Avatar.Root>
---
+++ src/lib/components/custom/navbar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    // import components
+    import SelectTeam from "./select-team.svelte";
+</script>
+
+
+<div class="w-full py-3 mb-6 px-80 border bg-card text-card-foreground shadow-sm">
+    <div class="flex flex-row justify-between">
+        <div class="flex flex-row items-center space-x-20">
+            <h1 class="text-2xl font-bold">Logo</h1>
+            <p class="text-sm text-muted-foreground">Docs</p>
+        </div>
+        <div class="flex flex-row items-center">
+            <SelectTeam />
+        </div>
+    </div>
+</div>
