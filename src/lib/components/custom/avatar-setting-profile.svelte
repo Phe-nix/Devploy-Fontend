f76064918e28c1b1c8ce3fed8391153f592dc53d
Thavith Javisooth <spicedexdee123@gmail.com>
2024-07-15 21:03:59 +0700

    💄  Create and Desgin components

--- src/lib/components/custom/avatar-setting-profile.svelte
+++ src/lib/components/custom/avatar-setting-profile.svelte
@@ -1,9 +1,39 @@
 <script lang="ts">
-    //import shadcn-svelte
-    import * as Avatar from "$lib/components/ui/avatar";
+  //import shadcn-svelte
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+  import * as Avatar from "$lib/components/ui/avatar";
+  import { Button } from "$lib/components/ui/button/index.js";
+
+  // import SVG
+  import { LogOut } from "lucide-svelte";
+  import { UserRound } from "lucide-svelte";
+  import { Settings } from 'lucide-svelte';
 </script>
 
-<Avatar.Root>
-    <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
-    <Avatar.Fallback>CN</Avatar.Fallback>
-  </Avatar.Root>
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger asChild let:builder>
+    <Button builders={[builder]} variant="ghost">
+      <Avatar.Root>
+        <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+        <Avatar.Fallback>CN</Avatar.Fallback>
+      </Avatar.Root>
+    </Button>
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content align="start" class="w-56">
+    <DropdownMenu.Label class="text-lg">My profile</DropdownMenu.Label>
+    <DropdownMenu.Separator />
+    <DropdownMenu.Item class="flex flex-row gap-x-2">
+      <UserRound class="text-primary"/>
+      Profile
+    </DropdownMenu.Item>
+    <DropdownMenu.Item class="flex flex-row gap-x-2">
+        <Settings class="text-primary"/>
+        Settings
+    </DropdownMenu.Item>
+    <DropdownMenu.Separator />
+    <DropdownMenu.Item class="text-destructive font-bold flex flex-row gap-x-2">
+      <LogOut />
+      Sign out
+    </DropdownMenu.Item>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
--- src/lib/components/custom/navbar.svelte
+++ src/lib/components/custom/navbar.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
     // import components
     import SelectTeam from "./select-team.svelte";
+    import AvatartSettingProfile from "./avatar-setting-profile.svelte";
+    import DarkLightMode from "./dark-light-mode.svelte";
+
+    // import shadcn-svelte
+    import { Button } from "$lib/components/ui/button";
 </script>
 
 
-<div class="w-full py-3 mb-6 px-80 border bg-card text-card-foreground shadow-sm">
+<div class="w-full py-2 mb-6 px-80 border-b bg-card text-card-foreground shadow-sm">
     <div class="flex flex-row justify-between">
         <div class="flex flex-row items-center space-x-20">
             <h1 class="text-2xl font-bold">Logo</h1>
-            <p class="text-sm text-muted-foreground">Docs</p>
+            <Button variant="ghost" class="text-sm font-medium text-muted-foreground">Docs</Button>
         </div>
-        <div class="flex flex-row items-center">
+        <div class="flex flex-row items-center space-x-5">
             <SelectTeam />
+            <AvatartSettingProfile />
+            <DarkLightMode />
         </div>
     </div>
 </div>
