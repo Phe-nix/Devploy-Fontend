74e639d78aa6ba939c03bf7946510d659c4d71e5
Thavith Javisooth <spicedexdee123@gmail.com>
2024-07-19 14:57:09 +0700

    💄 add and update style ui

---
+++ src/lib/components/custom/emtpy-projects.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  // import shadcn-svelte
+
+  // SVG Icons
+  import { PackageX } from "lucide-svelte";
+</script>
+
+<div class="flex flex-col items-center space-y-2 py-48">
+  <div class="border bg-card text-card-foreground p-3 rounded-lg shadow-xl">
+    <PackageX class="size-8" />
+  </div>
+  <div class="text-lg font-semibold">You don't have any project.</div>
+  <p class="text-sm text-muted-foreground">Let start project and create something new.</p>
+</div>
--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -1,9 +1,69 @@
 <script lang="ts">
-    // import components
-    
+  // import shadcn-svelte
+  import * as Card from "$lib/components/ui/card/index.js";
+  import * as Tabs from "$lib/components/ui/tabs";
+  import * as Command from "$lib/components/ui/command";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+
+  // import components
+  import EmptyProjects from "$lib/components/custom/emtpy-projects.svelte";
+
+  // SVG Icons
+  import { Plus } from "lucide-svelte";
+
+  const projects = [
+    {
+      name: "Project 1",
+      description: "Description 1",
+    },
+    {
+      name: "Project 2",
+      description: "Description 2",
+    },
+    {
+      name: "Project 3",
+      description: "Description 3",
+    },
+  ];
 </script>
 
-<div>
-    <h1>Home Page</h1>
-    <p>This is the home page.</p>
+<div class="max-w-7xl mx-auto">
+  <div class="flex flex-col">
+    <h2
+      class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+    >
+      Projects
+    </h2>
+    <p class="text-sm text-muted-foreground">Mannage your projects.</p>
+  </div>
+  <Tabs.Root value="projects" class="w-full py-10">
+    <Tabs.List class="grid w-full grid-cols-4">
+      <Tabs.Trigger value="projects">Projects</Tabs.Trigger>
+      <Tabs.Trigger value="password">Monitor</Tabs.Trigger>
+      <Tabs.Trigger value="password">Teams</Tabs.Trigger>
+      <Tabs.Trigger value="password">Setting</Tabs.Trigger>
+    </Tabs.List>
+    <Tabs.Content value="projects">
+      <Card.Root>
+        <Card.Header>
+          <form class="flex w-full max-w-5xl mx-auto items-center space-x-2">
+            <Input
+              class="shadow-lg p-4"
+              type="text"
+              placeholder="🔍 Search something..."
+            />
+            <Button class="space-x-2 flex items-center" type="submit">
+              <Plus class="mr-2 size-4" />
+              Create Projects
+            </Button>
+          </form>
+        </Card.Header>
+        <Card.Content>
+          <EmptyProjects />
+        </Card.Content>
+      </Card.Root>
+    </Tabs.Content>
+    <Tabs.Content value="password">Change your password here.</Tabs.Content>
+  </Tabs.Root>
 </div>
