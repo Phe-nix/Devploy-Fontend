9110563d2cc975902ed0e6ab7bf4137011aa9a51
Thavith Javisooth <spicedexdee123@gmail.com>
2024-07-28 00:26:50 +0700

    💄  new services page

---
+++ src/routes/(app)/home/project/[id]/services/[id]/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  // shadcn-svelte
+  import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
+  import * as Tabs from "$lib/components/ui/tabs";
+  import * as Card from "$lib/components/ui/card/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+
+  // import components
+  import ServicesStatus from "$lib/components/custom/Services-status.svelte";
+
+  // import SVG
+
+</script>
+
+<div>
+  <Breadcrumb.Root>
+    <Breadcrumb.List>
+      <Breadcrumb.Item>
+        <Breadcrumb.Link href="/home">Projects</Breadcrumb.Link>
+      </Breadcrumb.Item>
+      <Breadcrumb.Separator />
+      <Breadcrumb.Item>
+        <Breadcrumb.Link href="/">Project name</Breadcrumb.Link>
+      </Breadcrumb.Item>
+      <Breadcrumb.Separator />
+      <Breadcrumb.Page>Zela</Breadcrumb.Page>
+    </Breadcrumb.List>
+  </Breadcrumb.Root>
+
+  <div class="py-4 flex justify-between items-center">
+    <h2
+      class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+    >
+      Zela project
+    </h2>
+    <ServicesStatus status="running"/>
+  </div>
+</div>
+
+<Tabs.Root value="projects" class="w-full py-6">
+  <Tabs.List class="grid w-full grid-cols-4">
+    <Tabs.Trigger value="projects">Projects</Tabs.Trigger>
+    <Tabs.Trigger value="password">Monitor</Tabs.Trigger>
+    <Tabs.Trigger value="password">Teams</Tabs.Trigger>
+    <Tabs.Trigger value="password">Setting</Tabs.Trigger>
+  </Tabs.List>
+  <Tabs.Content value="projects">
+    <Card.Root>
+      <Card.Content
+        class="flex flex-row gap-3 flex-wrap items-center justify-center xl:justify-start"
+      >
+        Hi
+      </Card.Content>
+    </Card.Root>
+  </Tabs.Content>
+  <Tabs.Content value="password">Change your password here.</Tabs.Content>
+</Tabs.Root>
