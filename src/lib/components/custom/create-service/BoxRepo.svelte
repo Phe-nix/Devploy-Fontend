2b4ae7074adae46c98f44e56cd83158c9d156817
Thavith Javisooth <spicedexdee123@gmail.com>
2024-07-31 22:00:33 +0700

    💄 Crearte ui compo box select

---
+++ src/lib/components/custom/create-service/BoxRepo.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  // shadcn-svelte
+  import * as Avatar from "$lib/components/ui/avatar";
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<div
+  class="flex items-center gap-2 border bg-card text-card-foreground shadow-sm rounded-sm p-2 relative"
+>
+  <Avatar.Root class="m-1">
+    <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+    <Avatar.Fallback>CN</Avatar.Fallback>
+  </Avatar.Root>
+  <p>Zela-fontend</p>
+  <Button class="absolute right-0 m-2 font-semibold">Import</Button>
+</div>
