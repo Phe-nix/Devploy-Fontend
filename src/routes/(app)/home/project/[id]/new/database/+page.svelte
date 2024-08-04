2bb567306be4f71c5d75709717e883588544447d
Thavith Javisooth <spicedexdee123@gmail.com>
2024-08-04 20:33:35 +0700

    💄 main page creating service

---
+++ src/routes/(app)/home/project/[id]/new/database/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  // shadcn-svelte
+  import * as Card from "$lib/components/ui/card/index.js";
+  import { Button } from "$lib/components/ui/button/index.js";
+
+  // import components
+  import SelectDatabase from "$lib/components/custom/create-database/Select-database.svelte";
+  import FormConfigDatabase from "$lib/components/custom/create-database/Form-config-database.svelte";
+</script>
+
+<div class="py-2 flex flex-col">
+  <h2
+    class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+  >
+    Let build something new.
+  </h2>
+  <p class="text-muted-foreground text-sm">to deploy service</p>
+</div>
+<div class="px-8 py-4">
+  <!-- <SelectDatabase /> -->
+  <FormConfigDatabase />
+</div>
