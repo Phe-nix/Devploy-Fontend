f947c98a738faf97fe9c8d1a01b5bce4642aae62
Thavith Javisooth <spicedexdee123@gmail.com>
2024-08-10 19:41:27 +0700

    📱 update responsive

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div
-  class="h-full w-full my-44 flex flex-col items-center space-y-10 justify-center"
+  class="h-full w-full flex flex-col items-center space-y-10 my-48"
 >
   <div class="flex flex-col space-y-10 items-center">
     <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
@@ -16,14 +16,14 @@
       Sign in to Devploy
     </h1>
   </div>
-  <Card.Root class="p-28">
+  <Card.Root class="p-8 md:p-20">
     <Card.Content>
       <div class="flex flex-col items-center">
-        <Button class="my-2 p-[1.em]">
+        <Button href="/home" class="my-4 p-[1.5em]">
           <Mail class="mr-2 h-4 w-4" />
           Login with @kmitl.ac.th
         </Button>
-        <p class="text-base text-muted-foreground">Please use KMITL email to Sign in</p>
+        <p class="text-base text-muted-foreground">Please use <span class="font-bold">@KMITL</span> to Sign in</p>
       </div>
     </Card.Content>
   </Card.Root>
