ff6bcd7167cf48f70bc30e703665fc44d9fd0db0
Thavith Javisooth <spicedexdee123@gmail.com>
2024-08-11 20:46:54 +0700

    💄 update botton route

--- src/lib/components/custom/create-service/GithubConnect.svelte
+++ src/lib/components/custom/create-service/GithubConnect.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   // shadcn-svelte
   import { Button } from "$lib/components/ui/button";
+  import * as Dialog from "$lib/components/ui/dialog/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+  import { flyAndScale } from "$lib/utils";
 
   // SVG Icons
-  import { Github } from 'lucide-svelte';
-  
-  // import components
-  import DialogGithub from "./DialogGithub.svelte";
+  import { Github } from "lucide-svelte";
+  import { ArrowRight } from "lucide-svelte";
 
+  export let isprivateRepo;
+  export let isSignedInGithub;
+  let showDialog = false;
 </script>
 
 <div class="flex flex-col items-center space-y-6">
@@ -21,8 +26,43 @@
     src="https://cdn.icon-icons.com/icons2/2428/PNG/512/github_black_logo_icon_147128.png"
     alt="Logo github"
   />
-  <Button>Connecting with Github</Button>
+  <Button on:click={() => {
+    isSignedInGithub = true;
+  }}>Connecting with Github</Button>
 </div>
 <div class="py-8">
-    <DialogGithub />
+  <Dialog.Root bind:open={showDialog}>
+    <Dialog.Trigger class="text-muted-foreground text-xs md:text-sm flex items-center gap-1">
+      Import with Public Repository from GitHub
+      <ArrowRight class="size-4" />
+    </Dialog.Trigger>
+    <Dialog.Content transition={flyAndScale} class="sm:max-w-[425px]">
+      <Dialog.Header class="pb-6">
+        <Dialog.Title>Import a GitHub Repository</Dialog.Title>
+        <Dialog.Description>
+          Import your project from a private repository on GitHub.
+        </Dialog.Description>
+      </Dialog.Header>
+      <div class="flex w-full flex-col gap-2">
+        <Label for="url"
+          >Enter <span class="font-semibold">the URL of a Github repository</span>
+          to deploy it :</Label
+        >
+        <Input
+          type="url"
+          id="url"
+          placeholder="https://some-provider.come/some.organization/some-project"
+        />
+      </div>
+      <Dialog.Footer class="flex flex-row sm:justify-between justify-between items-center">
+        <Button on:click={() => {
+          showDialog = false;
+        }}>Back</Button>
+        <Button type="submit" on:click={() => {
+          showDialog = false;
+          isprivateRepo = true;
+        }}>Let Built</Button>
+      </Dialog.Footer>
+    </Dialog.Content>
+  </Dialog.Root>
 </div>
