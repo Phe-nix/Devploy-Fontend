302dabf929c18f438f25efdff7b4c8090fd550b6
Thavith Javisooth <spicedexdee123@gmail.com>
2024-08-11 20:47:13 +0700

    💄 fix ui and update reponsive

--- src/lib/components/custom/create-service/SelectRepo.svelte
+++ src/lib/components/custom/create-service/SelectRepo.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   // shadcn-svelte
   import { Input } from "$lib/components/ui/input/index.js";
+  import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
 
   // SVG Icons
   import { Github } from "lucide-svelte";
@@ -10,7 +11,7 @@
 </script>
 
 <div class="flex flex-col px-5 space-y-2 items-center">
-  <div class="flex flex-col space-y-3 w-1/2">
+  <div class="flex flex-col space-y-3 md:w-1/2 w-full">
     <div
       class="flex gap-2 justify-center rounded-lg border text-card-foreground shadow-xl w-28 py-2 dark:bg-zinc-100 bg-zinc-950"
     >
@@ -18,12 +19,19 @@
       <p class="dark:text-zinc-950 text-zinc-100">Phe-nix</p>
     </div>
     <Input type="email" placeholder="Search Projects..." />
-    <div class="overflow-auto max-h-[250px]">
+    <ScrollArea class="h-72 w-full rounded-md">
       <BoxRepo />
       <BoxRepo />
       <BoxRepo />
       <BoxRepo />
       <BoxRepo />
-    </div>
+    </ScrollArea>
+    <!-- <div class="overflow-y-hidden max-h-[250px]">
+      <BoxRepo />
+      <BoxRepo />
+      <BoxRepo />
+      <BoxRepo />
+      <BoxRepo />
+    </div> -->
   </div>
 </div>
