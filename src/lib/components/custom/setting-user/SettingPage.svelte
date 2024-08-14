4d8a8399ce1e743832e117cf7c24e32537be41dd
Thavith Javisooth <spicedexdee123@gmail.com>
2024-08-14 23:00:34 +0700

    💄 create page setting

---
+++ src/lib/components/custom/setting-user/SettingPage.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  // import shadcn-svelte
+  import * as Card from "$lib/components/ui/card";
+
+  // import components
+  import Sidebar from "./Sidebar.svelte";
+  import Profile from "./page/Profile.svelte";
+
+  let nowPageSide = "profile";
+
+  const sidebarNavItems = [
+    {
+      title: "Profile",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-user"><rect width="18" height="18" x="3" y="3" rx="2"/><circle cx="12" cy="10" r="3"/><path d="M7 21v-2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2"/></svg>`,
+    },
+    {
+      title: "Account",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-user"><circle cx="12" cy="12" r="10"/><circle cx="12" cy="10" r="3"/><path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662"/></svg>`,
+    },
+    {
+      title: "Appearance",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-palette"><circle cx="13.5" cy="6.5" r=".5" fill="currentColor"/><circle cx="17.5" cy="10.5" r=".5" fill="currentColor"/><circle cx="8.5" cy="7.5" r=".5" fill="currentColor"/><circle cx="6.5" cy="12.5" r=".5" fill="currentColor"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>`,
+    },
+    {
+      title: "Notifications",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"/><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"/></svg>`,
+    },
+  ];
+</script>
+
+<Card.Root>
+  <Card.Content>
+    <div class="hidden space-y-6 p-10 pb-16 md:block">
+      <div
+        class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0"
+      >
+        <aside class="-mx-4 lg:w-1/5">
+          <Sidebar items={sidebarNavItems} bind:nowPageSide/>
+        </aside>
+        <div class="flex-1 lg:max-w-4xl">
+          {#if nowPageSide === "profile"}
+            <Profile/>
+          {/if}
+        </div>
+      </div>
+    </div>
+  </Card.Content>
+</Card.Root>
