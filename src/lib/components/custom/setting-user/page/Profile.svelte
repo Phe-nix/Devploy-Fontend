fac318f8ac5b76a60c7dbec694866b24e9911ee6
Thavith Javisooth <spicedexdee123@gmail.com>
2024-08-14 23:01:06 +0700

    🚧 on working

---
+++ src/lib/components/custom/setting-user/page/Profile.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+</script>
+
+<div class="flex w-full max-w-sm flex-col gap-1.5">
+  <Label for="User">Username</Label>
+  <Input type="User" id="User" placeholder="User" />
+</div>
