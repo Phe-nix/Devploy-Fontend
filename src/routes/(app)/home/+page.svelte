5187a8ab782f24ce81b0612b870be1950fd400d9
Thavith Javisooth <spicedexdee123@gmail.com>
2024-08-14 23:02:01 +0700

    💄 update label in main page

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -9,9 +9,29 @@
   import Cardprojects from "$lib/components/custom/Card-projects.svelte";
   import ModalCreateProjects from "$lib/components/custom/Modal-create-projects.svelte";
   import TeamPage from "$lib/components/custom/tabs/teams/Team-page.svelte";
+  import SettingPage from "$lib/components/custom/setting-user/SettingPage.svelte";
 
   // SVG Icons
   import { Plus } from "lucide-svelte";
+  let check = "";
+
+  const tabs = [
+    {
+      name: "projects",
+      label: "Projects",
+      description: "Mannage your projects.",
+    },
+    {
+      name: "teams",
+      label: "Teams",
+      description: "Mannage your teams.",
+    },
+    {
+      name: "setting",
+      label: "Setting",
+      description: "Mannage your setting.",
+    },
+  ];
 
   const projects = [
     {
@@ -33,60 +53,28 @@
 </script>
 
 <div>
-  <!-- <div class="flex flex-col">
-    <h2
-      class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
-    >
-      Projects
-    </h2>
-    <p class="text-sm text-muted-foreground">Mannage your projects.</p>
-  </div> -->
-  <Tabs.Root value="projects" class="w-full py-10">
-    <Tabs.List class="grid w-full grid-cols-4">
+  <div class="flex flex-col">
+    {#each tabs as tab}
+    {#if tab.name === check}
+      <h2
+        class="scroll-m-20 pb-2 text-4xl font-semibold tracking-tight transition-colors first:mt-0"
+      >
+        {tab.label}
+      </h2>
+      <p class="text-sm text-muted-foreground">{tab.description}</p>
+    {/if}
+    {/each}
+  </div>
+  <Tabs.Root bind:value={check} class="w-full py-6">
+    <Tabs.List class="grid w-full grid-cols-3">
       <Tabs.Trigger value="projects">Projects</Tabs.Trigger>
-      <Tabs.Trigger value="monitor">Monitor</Tabs.Trigger>
       <Tabs.Trigger value="teams">Teams</Tabs.Trigger>
       <Tabs.Trigger value="setting">Setting</Tabs.Trigger>
     </Tabs.List>
     <Tabs.Content value="projects">
       <Card.Root>
         <Card.Header>
-          <form class="flex flex-col w-full max-w-5xl mx-auto space-y-4">
-            <div class="flex justify-between">
-              <div>
-                <h2 class="text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-                  Projects
-                </h2>
-                <p class="text-muted-foreground text-sm">Mannage your projects.</p>
-              </div>
-              <ModalCreateProjects />
-            </div>
-            <Input
-              class="shadow-lg p-4"
-              type="text"
-              placeholder="🔍 Search something..."
-            />
-          </form>
-        </Card.Header>
-        <Card.Content
-          class="flex flex-row gap-3 flex-wrap items-center justify-center xl:justify-start"
-        >
-          {#if projects === undefined}
-            <EmptyProjects />
-          {:else}
-            {#each projects as project}
-              <Cardprojects
-                {project}
-              />
-            {/each}
-          {/if}
-        </Card.Content>
-      </Card.Root>
-    </Tabs.Content>
-    <Tabs.Content value="monitor">
-      <Card.Root>
-        <Card.Header>
-          <form class="flex w-full max-w-5xl mx-auto items-center space-x-2">
+          <form class="flex w-full max-w-5xl mx-auto space-x-4">
             <Input
               class="shadow-lg p-4"
               type="text"
@@ -102,9 +90,7 @@
             <EmptyProjects />
           {:else}
             {#each projects as project}
-              <Cardprojects
-                {project}
-              />
+              <Cardprojects {project} />
             {/each}
           {/if}
         </Card.Content>
@@ -112,6 +98,9 @@
     </Tabs.Content>
     <Tabs.Content value="teams">
       <TeamPage />
+    </Tabs.Content>
+    <Tabs.Content value="setting">
+      <SettingPage />
     </Tabs.Content>
   </Tabs.Root>
 </div>
