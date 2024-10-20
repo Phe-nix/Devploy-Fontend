c542bf01f81423d242e4c8f722bef3607222a8dc
Phe-nix <spicedexdee123@gmail.com>
2024-10-20 19:15:56 +0700

    ⚡️ optimize code

--- src/lib/components/custom/button-create-service.svelte
+++ src/lib/components/custom/button-create-service.svelte
@@ -16,41 +16,16 @@
   export let type;
 </script>
 
-<!-- <DropdownMenu.Root>
-  <DropdownMenu.Trigger>
-    <Button class="gap-3 font-normal">
-      <Computer class="size-6" />
-      Create Services
-    </Button>
-  </DropdownMenu.Trigger>
-  <DropdownMenu.Content transition={flyAndScale} sideOffset={8}>
-    <DropdownMenu.Group>
-      <DropdownMenu.Item>
-        <a class="flex gap-3 items-center" href="/home/project/1/new/service">
-          <Server class="size-4" />
-          Appications
-        </a>
-      </DropdownMenu.Item>
-      <DropdownMenu.Item>
-        <a class="flex gap-3 items-center" href="/home/project/1/new/database">
-            <Database class="size-4" />
-            Databases
-        </a>
-      </DropdownMenu.Item>
-    </DropdownMenu.Group>
-  </DropdownMenu.Content>
-</DropdownMenu.Root> -->
-
 {#if type == 'application'}
 <Button on:click={() => {
-  goto('/home/new/service')
+  goto('/new/service')
 }} class={cn("gap-3 font-normal", className)}>
   <Server class="size-6" />
   Create Applications
 </Button>
 {:else if type == 'database'}
 <Button on:click={() => {
-  goto('/home/new/database')
+  goto('/new/database')
 }} class={cn("gap-3 font-normal", className)}>
   <Database class="size-6" />
   Create Databases
