3c3fa23b623cdba2453675f1a4db24b1daf4d683
Phe-nix <spicedexdee123@gmail.com>
2024-10-20 19:17:49 +0700

    💄 update form

--- src/lib/components/custom/setting-user/page/profile.svelte
+++ src/lib/components/custom/setting-user/page/profile.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
+  import * as Avatar from "$lib/components/ui/avatar/index.js";
   import { Input } from "$lib/components/ui/input/index.js";
   import { Label } from "$lib/components/ui/label/index.js";
 </script>
 
-<div class="flex w-full max-w-sm flex-col gap-1.5">
-  <Label for="User">Username</Label>
-  <Input type="User" id="User" placeholder="User" />
+<div class="grid space-y-5">
+  <Avatar.Root class="size-28">
+    <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+    <Avatar.Fallback>CN</Avatar.Fallback>
+  </Avatar.Root>
+  <div class="grid w-full max-w-sm items-center gap-1.5">
+    <Label for="picture">Profile</Label>
+    <Input id="picture" type="file" />
+  </div>
+  <div class="grid w-full max-w-sm items-center gap-1.5">
+    <Label for="User">Username</Label>
+    <Input type="User" id="User" placeholder="User" />
+  </div>
 </div>
