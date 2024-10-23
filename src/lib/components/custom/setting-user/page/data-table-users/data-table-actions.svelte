f6637c36655accf15bde2b7ce42b7f12689b9a58
Phe-nix <spicedexdee123@gmail.com>
2024-10-23 23:24:10 +0700

    💄 update detail data table in setting manage user

---
+++ src/lib/components/custom/setting-user/page/data-table-users/data-table-actions.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Ellipsis from "lucide-svelte/icons/ellipsis";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+  import { Button } from "$lib/components/ui/button";
+  export let id: string;
+</script>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger asChild let:builder>
+    <Button
+      variant="ghost"
+      builders={[builder]}
+      size="icon"
+      class="relative h-8 w-8 p-0"
+    >
+      <span class="sr-only">Open menu</span>
+      <Ellipsis class="h-4 w-4" />
+    </Button>
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content>
+    <DropdownMenu.Group>
+      <DropdownMenu.Label>Actions</DropdownMenu.Label>
+      <DropdownMenu.Item class="text-green-600">Edit</DropdownMenu.Item>
+      <DropdownMenu.Item class="text-red-600">Delete</DropdownMenu.Item>
+    </DropdownMenu.Group>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
---
+++ src/lib/components/custom/setting-user/page/data-table-users/modal-crate-user.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { Button, buttonVariants } from "$lib/components/ui/button/index.js";
+  import * as Dialog from "$lib/components/ui/dialog/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+
+  import { Plus } from "lucide-svelte";
+</script>
+
+<Dialog.Root>
+  <Dialog.Trigger class={buttonVariants({ variant: "default" })}>
+    <div class="flex items-center gap-2">
+      <Plus class="size-5"/>
+      Create User
+    </div>
+  </Dialog.Trigger>
+  <Dialog.Content class="sm:max-w-[425px]">
+    <Dialog.Header>
+      <Dialog.Title>Create user</Dialog.Title>
+      <Dialog.Description>
+        Make create new user. Click crate when you're done.
+      </Dialog.Description>
+    </Dialog.Header>
+    <div class="grid gap-4 py-4">
+      <div class="grid grid-cols-4 items-center gap-4">
+        <Label for="email" class="text-right">Email</Label>
+        <Input id="email" value="64070047@kmitl.ac.th" class="col-span-3" />
+      </div>
+      <div class="grid grid-cols-4 items-center gap-4">
+        <Label for="username" class="text-right">Username</Label>
+        <Input id="username" value="@Phe-nix" class="col-span-3" />
+      </div>
+    </div>
+    <Dialog.Footer>
+      <Button type="submit">Save changes</Button>
+    </Dialog.Footer>
+  </Dialog.Content>
+</Dialog.Root>
