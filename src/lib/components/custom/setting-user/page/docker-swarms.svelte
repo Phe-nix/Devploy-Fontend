1e0e01e4dac95ec355786e94f99666f7379d0c57
Phe-nix <spicedexdee123@gmail.com>
2024-10-23 23:23:19 +0700

    💄 detail new page setting

---
+++ src/lib/components/custom/setting-user/page/docker-swarms.svelte
@@ -0,0 +1,11 @@
+
+<div>
+    <div class="flex flex-col space-y-2">
+        <h4 class="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Docker Swarms
+        </h4>
+        <p class="text-muted-foreground text-sm">
+          Manage your cluster in docker swarms
+        </p>
+      </div>
+</div>
