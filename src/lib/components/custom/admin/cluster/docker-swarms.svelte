956840daa6e5b549ca5f8317199ff7e84eff1365
Phe-nix <spicedexdee123@gmail.com>
2024-11-01 00:59:25 +0700

    💄 change stype text

--- src/lib/components/custom/admin/cluster/docker-swarms.svelte
+++ src/lib/components/custom/admin/cluster/docker-swarms.svelte
@@ -1,9 +1,11 @@
 
 <div>
     <div class="flex flex-col space-y-2">
-        <h4 class="scroll-m-20 text-2xl font-semibold tracking-tight">
-          Docker Swarms
-        </h4>
+      <h2
+      class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+    >
+      Docker Swarms
+    </h2>
         <p class="text-muted-foreground text-sm">
           Manage your cluster in docker swarms
         </p>
