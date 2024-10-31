e1ff634b461040596ec7e1a3c16e8406720c62a2
Phe-nix <spicedexdee123@gmail.com>
2024-11-01 01:00:03 +0700

    💄 update componete

--- src/lib/components/custom/admin/users/data-table-users-action.svelte
+++ src/lib/components/custom/admin/users/data-table-users-action.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button/index.js";
-   </script>
-    
-    <div class="flex gap-4">
-        <Button size="sm">Edit</Button>
-        <Button size="sm" variant="secondary">Delete</Button>
+  import Ellipsis from "lucide-svelte/icons/ellipsis";
+  import * as Popover from "$lib/components/ui/popover";
+  import AdminEditUser from "./dialog-user/admin-edit-user.svelte";
+  import AdminDeleteUser from "./dialog-user/admin-delete-user.svelte";
+  export let id: string;
+</script>
+
+<Popover.Root>
+  <Popover.Trigger class="rounded-lg">
+    <Ellipsis class="h-4 w-4" />
+  </Popover.Trigger>
+  <Popover.Content class="w-18">
+    <div class="flex flex-col items-center text-center z-40">
+      <AdminEditUser />
+      <AdminDeleteUser />
     </div>
+  </Popover.Content>
+</Popover.Root>
